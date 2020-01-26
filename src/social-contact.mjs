1dae49ad35e41fe58db592ae4c101d56e2e29c38
Richie Casto <rcasto92@gmail.com>
2020-01-26 09:49:45 -0800

    add id to rendered social contact methods

--- src/social-contact.mjs
+++ src/social-contact.mjs
@@ -31,6 +31,7 @@ function constructSocialContact(socialContact) {
     const socialAnchor = document.createElement('a');
     socialAnchor.href = socialContact.profile;
     socialAnchor.target = '_blank';
+    socialAnchor.id = socialContact.id;
 
     const socialImage = document.createElement('img');
     socialImage.src = socialContact.brandImage;
@@ -42,6 +43,7 @@ function constructSocialContact(socialContact) {
 
 function constructGithubContact(username) {
     return {
+        id: 'github',
         brandImage: githubImage,
         profile: `https://github.com/${username}`
     };
@@ -49,6 +51,7 @@ function constructGithubContact(username) {
 
 function constructLinkedinContact(username) {
     return {
+        id: 'linkedin',
         brandImage: linkedinImage,
         profile: `https://www.linkedin.com/in/${username}`
     };
@@ -56,6 +59,7 @@ function constructLinkedinContact(username) {
 
 function constructTwitterContact(username) {
     return {
+        id: 'twitter',
         brandImage: twitterImage,
         profile: `https://twitter.com/${username}`
     };
@@ -63,6 +67,7 @@ function constructTwitterContact(username) {
 
 function constructFacebookContact(username) {
     return {
+        id: 'facebook',
         brandImage: facebookImage,
         profile: `https://www.facebook.com/${username}`
     };
@@ -70,59 +75,60 @@ function constructFacebookContact(username) {
 
 function constructInstagramContact(username) {
     return {
+        id: 'instagram',
         brandImage: instagramImage,
         profile: `https://www.instagram.com/${username}`
     };
 }
 
 class SocialContact extends HTMLElement {
-  constructor() {
-    super();
-  }
-  connectedCallback() {
-    const shadowRoot = this.attachShadow({
-      mode: 'open'
-    });
-    const socialContactTemplateClone = template.content.cloneNode(true);
-    const socialContactContainer = socialContactTemplateClone.querySelector('.social-contact-container');
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({
+            mode: 'open'
+        });
+        const socialContactTemplateClone = template.content.cloneNode(true);
+        const socialContactContainer = socialContactTemplateClone.querySelector('.social-contact-container');
 
-    const github = this.getAttribute('github') || '';
-    const linkedin = this.getAttribute('linkedin') || '';
-    const twitter = this.getAttribute('twitter') || '';
-    const facebook = this.getAttribute('facebook') || '';
-    const instagram = this.getAttribute('instagram') || '';
+        const github = this.getAttribute('github') || '';
+        const linkedin = this.getAttribute('linkedin') || '';
+        const twitter = this.getAttribute('twitter') || '';
+        const facebook = this.getAttribute('facebook') || '';
+        const instagram = this.getAttribute('instagram') || '';
 
-    const socialContacts = [];
+        const socialContacts = [];
 
-    if (github) {
-        socialContacts.push(constructGithubContact(github));
-    }
-    if (linkedin) {
-        socialContacts.push(constructLinkedinContact(linkedin));
-    }
-    if (twitter) {
-        socialContacts.push(constructTwitterContact(twitter));
-    }
-    if (facebook) {
-        socialContacts.push(constructFacebookContact(facebook));
-    }
-    if (instagram) {
-        socialContacts.push(constructInstagramContact(instagram));
-    }
+        if (github) {
+            socialContacts.push(constructGithubContact(github));
+        }
+        if (linkedin) {
+            socialContacts.push(constructLinkedinContact(linkedin));
+        }
+        if (twitter) {
+            socialContacts.push(constructTwitterContact(twitter));
+        }
+        if (facebook) {
+            socialContacts.push(constructFacebookContact(facebook));
+        }
+        if (instagram) {
+            socialContacts.push(constructInstagramContact(instagram));
+        }
 
-    if (socialContacts.length) {
-        socialContacts
-            .forEach(socialContact => {
-                const socialContactElem = constructSocialContact(socialContact);
-                socialContactContainer.appendChild(socialContactElem);
-            });
-    } else {
-        const socialContactText = document.createTextNode('No social profile information provided');
-        socialContactContainer.appendChild(socialContactText);
-    }
+        if (socialContacts.length) {
+            socialContacts
+                .forEach(socialContact => {
+                    const socialContactElem = constructSocialContact(socialContact);
+                    socialContactContainer.appendChild(socialContactElem);
+                });
+        } else {
+            const socialContactText = document.createTextNode('No social profile information provided');
+            socialContactContainer.appendChild(socialContactText);
+        }
 
-    shadowRoot.appendChild(socialContactTemplateClone);
-  }
+        shadowRoot.appendChild(socialContactTemplateClone);
+    }
 }
 
 customElements.define('social-contact', SocialContact);
