fbb8fc07b597412f7c8857942b8bf1b0bc4cbbcf
Richie Casto <rcasto92@gmail.com>
2020-05-09 21:41:42 -0700

    add and integrate @rollup/plugin-image

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,8 +1,12 @@
 import babel from 'rollup-plugin-babel';
+import image from '@rollup/plugin-image';
 
 const config = {
 	input: 'src/social-contact.js',
-	plugins: [babel()],
+	plugins: [
+		babel(),
+		image()
+	],
 };
 
 export default config;
